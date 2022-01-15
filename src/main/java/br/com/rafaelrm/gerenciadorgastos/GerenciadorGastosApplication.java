6bb9c600d47f87ba55342e6c078fb1392b2f7013
Rafael Ramos Machado <rafa.ramosmachado@gmail.com>
2022-01-15 01:08:18 -0300

    Criação do projeto Gerenciador de Gastos

---
+++ src/main/java/br/com/rafaelrm/gerenciadorgastos/GerenciadorGastosApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelrm.gerenciadorgastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorGastosApplication.class, args);
+	}
+
+}
